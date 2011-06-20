5b4ee34af67f0d9616c2c6e1588f6896c306dbcf
bombax <bombax@localhost>
2011-06-20 21:45:43 +0000

    Terminal node search

---
+++ src/Phylopp/Searching/SearchNode.h
@@ -0,0 +1,64 @@
+#ifndef SEARCHNODE_H
+#define SEARCHNODE_H
+
+#include "Phylopp/Traversal/Traverser.h"
+#include "Phylopp/Traversal/NodeVisitor.h"
+
+using namespace Traversal;
+
+namespace Searching
+{
+    template <class T>
+    struct IsLeafPredicate
+    {
+	bool operator()(T* node) const
+	{
+	    return node->isLeaf();
+	}
+    };
+	  
+    template <class T>
+    class SelectNodeAction
+    {
+    public:
+	SelectNodeAction(std::string & searchString)
+	{
+	    regExp=searchString;
+	}
+	
+	VisitAction visitNode(T* n)
+	{
+	    if(n->getLocation().find(regExp,0)!=string::npos){
+		n->setSelected(true);
+		n->update();
+	    }
+	    return ContinueTraversing;
+	}
+    private:
+	std::string regExp;  
+    };
+    
+    
+    template <class T>
+    class SearchNode : public T
+    {
+      public:
+	  void setRoot(T * root)
+	  {
+	      node=root;
+	  }
+	  
+	  void search(std::string & expression)
+	  {
+	      Traverser<T, SelectNodeAction<T> , IsLeafPredicate<T> > t;
+	      SelectNodeAction<T> a(expression);
+
+	      t.traverseDescendants(node, a);
+	  }
+      private:
+	  T * node;
+    };
+   
+}
+
+#endif
