2071d6524cb671617620aac3771a567fd4456fcc
emmanuelteisaire <emmanuelteisaire@localhost>
2011-08-16 22:14:30 +0000

    license on DataSource files

--- src/Phylopp/DataSource/DistancesParser.h
+++ src/Phylopp/DataSource/DistancesParser.h
@@ -1,3 +1,25 @@
+/*
+DistancesParser: A parser for loading location distances from a file
+
+    Copyright (C) 2011 Emmanuel Teisaire, Nicolás Bombau, Carlos Castro, Damián Domé, FuDePAN
+
+    This file is part of the Phyloloc project.
+
+    Phyloloc is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Phyloloc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Phyloloc.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef DISTANCES_PARSER_H
 #define DISTANCES_PARSER_H
 
--- src/Phylopp/DataSource/IDataSourceStrategy.h
+++ src/Phylopp/DataSource/IDataSourceStrategy.h
@@ -1,3 +1,26 @@
+/*
+IDataSourceStrategy: Interface for loading and saving trees from and to a particular
+DataSource
+              
+    Copyright (C) 2011 Emmanuel Teisaire, Nicolás Bombau, Carlos Castro, Damián Domé, FuDePAN
+
+    This file is part of the Phyloloc project.
+
+    Phyloloc is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Phyloloc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Phyloloc.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef IDATASOURCESTRATEGY_H
 #define IDATASOURCESTRATEGY_H
 
