95f6942360d30cc2bc574f55fb57cce43abb6407
elsilmaril <elsilmaril@gmail.com>
2012-01-08 16:54:55 +0000

    fixes issue 82 Nicolas' changes on GUI graphwidget files

--- src/PhyloGUI/inc/graphwidget.h
+++ src/PhyloGUI/inc/graphwidget.h
@@ -1,21 +1,21 @@
 /*
-    Copyright (C) 2011 Emmanuel Teisaire, Nicolás Bombau, Carlos Castro, Damián Domé, FuDePAN
-
-    This file is part of the Phyloloc project.
-
-    Phyloloc is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Phyloloc is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Phyloloc.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ C o*pyright (C) 2011 Emmanuel Teisaire, Nicolás Bombau, Carlos Castro, Damián Domé, FuDePAN
+ 
+ This file is part of the Phyloloc project.
+ 
+ Phyloloc is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Phyloloc is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Phyloloc.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef GRAPHWIDGET_H
 #define GRAPHWIDGET_H
 
@@ -38,39 +38,40 @@
 
 namespace PhyloGUI
 {
-
-class GraphWidget : public QGraphicsView
-{
-
-public:
-    GraphWidget(Locations::LocationManager& locationManager, QWidget* parent = 0);
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent* event);
-    void mousePressEvent(QGraphicsSceneMouseEvent* event);
-    void paintNode(QColor color, Domain::ITree<GuiNode>* tree);
-    void draw(Domain::ITree<GuiNode>* tree);
-    void draw(void);
-    void unSelectAllNodes(Domain::ITree<GuiNode>* tree);
-    void selectAllNodes(Domain::ITree<GuiNode>* tree);
-    void selectNodeDescendants(Domain::ITree<GuiNode>* tree);
-    void selectNodeAncestors(Domain::ITree<GuiNode>*tree);
-    void scaleView(qreal scaleFactor);
-    Locations::LocationManager& getLocationManager() const;
-    QSize sizeHint() const;
-
-protected:
-    void wheelEvent(QWheelEvent* event);
-    void drawBackground(QPainter* painter, const QRectF& rect);
-    void resizeEvent(QResizeEvent* event);
-
-private:
-    Domain::ITree<GuiNode>* tree;
-    int timerId;
-    Locations::LocationManager& lm;
-    void drawTree(QGraphicsScene* scene, GuiNode* node);
-    QPointF drawTreeAux(QGraphicsScene* scene, GuiNode* node, float depth, unsigned int* leafNumber);
-    void reScaleScene();
-};
-
+    
+    class GraphWidget : public QGraphicsView
+    {
+        
+    public:
+        GraphWidget(Locations::LocationManager& locationManager, QWidget* parent = 0);
+        ~GraphWidget();
+        void mouseReleaseEvent(QGraphicsSceneMouseEvent* event);
+        void mousePressEvent(QGraphicsSceneMouseEvent* event);
+        void paintNode(QColor color, Domain::ITree<GuiNode>* tree);
+        void draw(Domain::ITree<GuiNode>* tree);
+        void draw(void);
+        void unSelectAllNodes(Domain::ITree<GuiNode>* tree);
+        void selectAllNodes(Domain::ITree<GuiNode>* tree);
+        void selectNodeDescendants(Domain::ITree<GuiNode>* tree);
+        void selectNodeAncestors(Domain::ITree<GuiNode>*tree);
+        void scaleView(qreal scaleFactor);
+        Locations::LocationManager& getLocationManager() const;
+        QSize sizeHint() const;
+        
+    protected:
+        void wheelEvent(QWheelEvent* event);
+        void drawBackground(QPainter* painter, const QRectF& rect);
+        void resizeEvent(QResizeEvent* event);
+        
+    private:
+        Domain::ITree<GuiNode>* tree;
+        int timerId;
+        Locations::LocationManager& lm;
+        void drawTree(QGraphicsScene* scene, GuiNode* node);
+        QPointF drawTreeAux(QGraphicsScene* scene, GuiNode* node, float depth, unsigned int* leafNumber);
+        void reScaleScene();
+    };
+    
 }
 
 #endif
